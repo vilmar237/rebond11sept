1a0b52d5289eaabc237def4870023d27d98b8060
Cyrille <mbia1378@gmail.com>
2022-07-18 15:39:27 +0100

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use \Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // On défini le "français" comme langue globale de l'application
         \App::setLocale('fr');
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
